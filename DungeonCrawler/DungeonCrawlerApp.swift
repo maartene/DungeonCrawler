d3f87fb55accc0d332a7fd68a2507729d948254a
Maarten Engels <maartene@thedreamweb.eu>
2025-02-22 13:23:33 +0100

    Initial Commit

---
+++ DungeonCrawler/DungeonCrawlerApp.swift
@@ -0,0 +1,17 @@
+//
+//  DungeonCrawlerApp.swift
+//  DungeonCrawler
+//
+//  Created by Maarten Engels on 22/02/2025.
+//
+
+import SwiftUI
+
+@main
+struct DungeonCrawlerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ DungeonCrawlerTests/DungeonCrawlerTests.swift
@@ -0,0 +1,17 @@
+//
+//  DungeonCrawlerTests.swift
+//  DungeonCrawlerTests
+//
+//  Created by Maarten Engels on 22/02/2025.
+//
+
+import Testing
+@testable import DungeonCrawler
+
+struct DungeonCrawlerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}

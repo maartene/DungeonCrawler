6901ca0e8eb457c35c43178a1ca6b21a61334b7f
Maarten Engels <maartene@thedreamweb.eu>
2025-04-06 14:21:19 +0200

    R: safe remove walls and stairs properties from Map

--- DungeonCrawler/GameLogic/Map.swift
+++ DungeonCrawler/GameLogic/Map.swift
@@ -22,42 +22,27 @@ enum Tile {
 }
 
 struct Map {
-    private var walls = Set<Coordinate>()
-    private var stairsUp = Set<Coordinate>()
-    private var stairsDown = Set<Coordinate>()
     private var tiles = [Coordinate: Tile]()
     
     var minX: Int {
-        walls.map { $0.x }.min() ?? 0
+        tiles.keys.map { $0.x }.min() ?? 0
     }
     
     var minY: Int {
-        walls.map { $0.y }.min() ?? 0
+        tiles.keys.map { $0.y }.min() ?? 0
     }
     
     var maxX: Int {
-        walls.map { $0.x }.max() ?? 0
+        tiles.keys.map { $0.x }.max() ?? 0
     }
     
     var maxY: Int {
-        walls.map { $0.y }.max() ?? 0
+        tiles.keys.map { $0.y }.max() ?? 0
     }
     
     init(_ mapArray: [[Character]] = [[]]) {
         for row in 0 ..< mapArray.count {
             for column in 0 ..< mapArray[0].count {
-                if mapArray[row][column] == "#" {
-                    walls.insert(Coordinate(x: column, y: row))
-                }
-                
-                if mapArray[row][column] == "<" {
-                    stairsUp.insert(Coordinate(x: column, y: row))
-                }
-                
-                if mapArray[row][column] == ">" {
-                    stairsDown.insert(Coordinate(x: column, y: row))
-                }
-                
                 tiles[Coordinate(x: column, y: row)] = Tile.characterToTile(mapArray[row][column])
             }
         }

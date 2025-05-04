b4f501af65e79647a5f7fb36f68f2561bf3cdbf0
Maarten Engels <maartene@thedreamweb.eu>
2025-05-04 14:25:08 +0200

    Show different floors

--- DungeonCrawler/Presentation/GameView.swift
+++ DungeonCrawler/Presentation/GameView.swift
@@ -26,45 +26,7 @@ struct GameView: NSViewRepresentable {
         // doesn't need anything yet
     }
     
-    private func createCube(worldPosition: SIMD3<Float>) -> AnchorEntity {
-        let color = NSColor(hue: CGFloat.random(in: 0...1), saturation: 1, brightness: 1, alpha: 1)
-        var cubeMaterial = SimpleMaterial(color: color, isMetallic: true)
-        cubeMaterial.metallic = 0.2
-        cubeMaterial.roughness = 0.7
-        let cubeEntity = Entity(components: [ModelComponent(mesh: .generateBox(size: 1), materials: [cubeMaterial])])
-        let cubeAnchor = AnchorEntity(world: worldPosition)
-        cubeAnchor.addChild(cubeEntity)
-        
-        return cubeAnchor
-    }
-    
-    private func placeModelAt(model: String, worldPosition: SIMD3<Float>) -> AnchorEntity {
-        guard let entity = try? Entity.load(named: model) else {
-            fatalError("Failed to load model - \(model)")
-        }
-        let anchor = AnchorEntity(world: worldPosition)
-        anchor.addChild(entity)
-        
-        return anchor
-    }
-    
     private func setupView(_ arView: ARView) {
-        for row in world.currentFloor.minY ... world.currentFloor.maxY {
-            for col in world.currentFloor.minX ... world.currentFloor.maxX {
-                let coordinate = Coordinate(x: col, y: row)
-                switch world.currentFloor.tileAt(coordinate) {
-                case .wall:
-                    arView.scene.addAnchor(placeModelAt(model: "Wall", worldPosition: coordinate.toSIMD3))
-                case .stairsUp:
-                    arView.scene.addAnchor(placeModelAt(model: "stairsUp", worldPosition: coordinate.toSIMD3))
-                case .stairsDown:
-                    arView.scene.addAnchor(placeModelAt(model: "stairsDown", worldPosition: coordinate.toSIMD3))
-                default:
-                    arView.scene.addAnchor(placeModelAt(model: "FloorTile", worldPosition: coordinate.toSIMD3))
-                }
-            }
-        }
-        
         guard let skyboxResource = try? EnvironmentResource.load(named: "ambientLight") else {
             fatalError("Unable to load skybox resource")
         }
--- DungeonCrawler/Presentation/WorldUpdateSystem.swift
+++ DungeonCrawler/Presentation/WorldUpdateSystem.swift
@@ -6,6 +6,7 @@
 //
 
 import RealityKit
+import SwiftUI
 
 struct WorldComponent: Component {
     let world: World
@@ -15,9 +16,11 @@ struct WorldComponent: Component {
 class WorldUpdateSystem: System {
     var world: World!
     var cameraAnchor: AnchorEntity!
+    private var currentRenderedFloor: Map?
+    private var mapAnchor: AnchorEntity?
     
     // Initializer is required. Use an empty implementation if there's no setup needed.
-    required init(scene: Scene) {
+    required init(scene: RealityKit.Scene) {
         guard let worldEntity = scene.findEntity(named: "WorldEntity") else {
             fatalError("Can't find WorldEntity in scene.")
         }
@@ -41,5 +44,57 @@ class WorldUpdateSystem: System {
     func update(context: SceneUpdateContext) {
         cameraAnchor.position = world.partyPosition.toSIMD3
         cameraAnchor.transform.rotation = world.partyHeading.toFloatQuaternion
+        
+        if currentRenderedFloor != world.currentFloor {
+            renderMap(scene: context.scene)
+        }
+    }
+    
+    private func renderMap(scene: RealityKit.Scene) {
+        if let mapAnchor {
+            scene.removeAnchor(mapAnchor)
+        }
+        
+        mapAnchor = AnchorEntity(world: .zero)
+        scene.addAnchor(mapAnchor!)
+        currentRenderedFloor = world.currentFloor
+        
+        for row in world.currentFloor.minY ... world.currentFloor.maxY {
+            for col in world.currentFloor.minX ... world.currentFloor.maxX {
+                let coordinate = Coordinate(x: col, y: row)
+                switch world.currentFloor.tileAt(coordinate) {
+                case .wall:
+                    mapAnchor?.addChild(placeModelAt(model: "Wall", worldPosition: coordinate.toSIMD3))
+                case .stairsUp:
+                    mapAnchor?.addChild(placeModelAt(model: "stairsUp", worldPosition: coordinate.toSIMD3))
+                case .stairsDown:
+                    mapAnchor?.addChild(placeModelAt(model: "stairsDown", worldPosition: coordinate.toSIMD3))
+                default:
+                    mapAnchor?.addChild(placeModelAt(model: "FloorTile", worldPosition: coordinate.toSIMD3))
+                }
+            }
+        }
+    }
+    
+    private func createCube(worldPosition: SIMD3<Float>) -> AnchorEntity {
+        let color = NSColor(hue: CGFloat.random(in: 0...1), saturation: 1, brightness: 1, alpha: 1)
+        var cubeMaterial = SimpleMaterial(color: color, isMetallic: true)
+        cubeMaterial.metallic = 0.2
+        cubeMaterial.roughness = 0.7
+        let cubeEntity = Entity(components: [ModelComponent(mesh: .generateBox(size: 1), materials: [cubeMaterial])])
+        let cubeAnchor = AnchorEntity(world: worldPosition)
+        cubeAnchor.addChild(cubeEntity)
+        
+        return cubeAnchor
+    }
+    
+    private func placeModelAt(model: String, worldPosition: SIMD3<Float>) -> AnchorEntity {
+        guard let entity = try? Entity.load(named: model) else {
+            fatalError("Failed to load model - \(model)")
+        }
+        let anchor = AnchorEntity(world: worldPosition)
+        anchor.addChild(entity)
+        
+        return anchor
     }
 }
